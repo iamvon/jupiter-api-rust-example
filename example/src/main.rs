b426a735515893dae56c4d7f808dd1569ff2a235
Tuan Pham Minh <tuanpmhd@gmail.com>
2024-01-29 22:41:58 +0700

    update

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,31 +1,39 @@
 use std::env;
+use std::future::IntoFuture;
 
 use jupiter_swap_api_client::{
     quote::QuoteRequest, swap::SwapRequest, transaction_config::TransactionConfig,
     JupiterSwapApiClient,
 };
 use solana_client::nonblocking::rpc_client::RpcClient;
+use solana_sdk::signer::Signer;
+use solana_sdk::{hash::Hash, pubkey::Pubkey, signature::NullSigner};
 use solana_sdk::{pubkey, transaction::VersionedTransaction};
-use solana_sdk::{pubkey::Pubkey, signature::NullSigner};
 use tokio;
 
 const USDC_MINT: Pubkey = pubkey!("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v");
 const NATIVE_MINT: Pubkey = pubkey!("So11111111111111111111111111111111111111112");
+const WENT_MINT: Pubkey = pubkey!("WENWENvqqNya429ubCdR81ZmD69brwQaaBYY6p3LCpk");
 
-pub const TEST_WALLET: Pubkey = pubkey!("2AQdpHJ2JpcEgPiATUXjQxA8QmafFegfQwSLWSprPicm"); // Coinbase 2 wallet
+const KEYPAIR_PATH: &str = ""; // change to your keypair path
+const RPC_URL: &str = "https://api.mainnet-beta.solana.com"; // change to your RPC URL
 
 #[tokio::main]
 async fn main() {
+    let file = std::fs::File::open(KEYPAIR_PATH).unwrap();
+    let data: serde_json::Value = serde_json::from_reader(file).unwrap();
+    let key_bytes: Vec<u8> = serde_json::from_value(data.clone()).unwrap();
+    let trading_keypair = solana_sdk::signature::Keypair::from_bytes(&key_bytes).unwrap();
+
     let api_base_url = env::var("API_BASE_URL").unwrap_or("https://quote-api.jup.ag/v6".into());
-    println!("Using base url: {}", api_base_url);
 
     let jupiter_swap_api_client = JupiterSwapApiClient::new(api_base_url);
 
     let quote_request = QuoteRequest {
         amount: 1_000_000,
-        input_mint: USDC_MINT,
-        output_mint: NATIVE_MINT,
-        slippage_bps: 50,
+        input_mint: NATIVE_MINT,
+        output_mint: WENT_MINT,
+        slippage_bps: 1000,
         ..QuoteRequest::default()
     };
 
@@ -36,7 +44,7 @@ async fn main() {
     // POST /swap
     let swap_response = jupiter_swap_api_client
         .swap(&SwapRequest {
-            user_public_key: TEST_WALLET,
+            user_public_key: trading_keypair.pubkey(),
             quote_response: quote_response.clone(),
             config: TransactionConfig::default(),
         })
@@ -45,18 +53,29 @@ async fn main() {
 
     println!("Raw tx len: {}", swap_response.swap_transaction.len());
 
-    let versioned_transaction: VersionedTransaction =
+    let mut versioned_transaction: VersionedTransaction =
         bincode::deserialize(&swap_response.swap_transaction).unwrap();
 
-    // Replace with a keypair or other struct implementing signer
-    let null_signer = NullSigner::new(&TEST_WALLET);
+    // Send with rpc client...
+    let rpc_client = RpcClient::new(RPC_URL.into());
+
+    // Fetch the latest blockhash
+    let recent_blockhash: Hash = match rpc_client.get_latest_blockhash().await {
+        Ok(blockhash) => blockhash,
+        Err(err) => {
+            eprintln!("Error fetching latest blockhash: {:?}", err);
+            return;
+        }
+    };
+
+    versioned_transaction
+        .message
+        .set_recent_blockhash(recent_blockhash);
+
     let signed_versioned_transaction =
-        VersionedTransaction::try_new(versioned_transaction.message, &[&null_signer]).unwrap();
+        VersionedTransaction::try_new(versioned_transaction.message, &[&trading_keypair]).unwrap();
 
-    // send with rpc client...
-    let rpc_client = RpcClient::new("https://api.mainnet-beta.solana.com".into());
-
-    // This will fail with "Transaction signature verification failure" as we did not really sign
+    // Broadcast transaction to solana blockchain
     let error = rpc_client
         .send_and_confirm_transaction(&signed_versioned_transaction)
         .await
@@ -66,7 +85,7 @@ async fn main() {
     // POST /swap-instructions
     let swap_instructions = jupiter_swap_api_client
         .swap_instructions(&SwapRequest {
-            user_public_key: TEST_WALLET,
+            user_public_key: trading_keypair.pubkey(),
             quote_response,
             config: TransactionConfig::default(),
         })
